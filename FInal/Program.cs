4f8c665ea354ffdb955f3ecfe7b083912584adf0
Chad <chadmc@outlook.com>
2016-08-06 10:06:38 -0700

    First 2 questions done.

---
+++ FInal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FInal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter text:");
+            string txt = Console.ReadLine();
+
+            char[] charlist = txt.ToCharArray();
+
+            var distchars = charlist.Distinct();
+
+            foreach (char distchar in distchars)
+            {
+                Console.Write(distchar);
+            }
+
+
+            // This is added to prevent the window from closing automatically
+            // Serves no other purpose
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Final2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "../../example.txt";
+            string line;
+            List<string> lines = new List<string>();
+
+            // load data
+             StreamReader reader = new StreamReader(file);
+            using (reader)
+            {
+                int i = 0;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    i++;
+                    lines.Add(i + " " + line);
+                }
+            }
+            reader.Close();
+
+            using (StreamWriter sw = new StreamWriter(file))
+            {
+                foreach (string l in lines)
+                {
+                    sw.WriteLine(l);
+                }
+                sw.Close();
+            }
+
+
+
+            // This is added to prevent the window from closing automatically
+            // Serves no other purpose
+            Console.ReadKey();
+        }
+    }
+}
